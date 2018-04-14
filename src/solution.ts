044baf0d296218ea3b85b1cddca123bc69e5b140
Alex Luecke <alex.james.luecke@gmail.com>
2018-04-14 14:32:00 -0700

    fix comment

--- src/solution.ts
+++ src/solution.ts
@@ -1,7 +1,6 @@
 export function solution(input: string): string[] {
   const array = input.split('');
 
-  // Read back to front with `pop` -> reverse array
   const result1 = buildResultOutput(zerosThenOnes([ ...array ]), '0', '1');
   const result2 = buildResultOutput(onesThenZeros([ ...array ]), '1', '0');
 
@@ -17,14 +16,15 @@ function onesThenZeros(array: string[]): number[] {
 }
 
 function scannerFactory(start: string, end: string): (input: string[]) => number[] {
+  /*
+   * @returns {number[]} A number array containing the number of sequential characters. E.g. if '0' is the starting
+   * character, '0011' will return [ 2 ], '0101' will return [ 1, 1 ].
+   */
   return (array: string[]) => {
     // Procedure:
     // 1. Find "start" character and count sequence
     // 2. Find "end" character and count sequence
     // 3. Take the min between the two numbers and ensure "start" was previously found
-    // Result:
-    // A number array containing the number of sequential characters.
-    // e.g. if '0' is the starting character, '0011' will return [ 2 ], '0101' will return [ 1, 1 ].
     let m = 0, n = 0;
 
     return array.reduce((result, char, i, A) => {
@@ -48,8 +48,8 @@ function scannerFactory(start: string, end: string): (input: string[]) => number
 
 function buildResultOutput(results: number[], start: string, end: string): string[] {
   // Observation:
-  // Every k sequence of a char, there will be k - 1 substrings.
-  // Instead of counting along the way, I build this result after gathering the sequences.
+  // Every k sequence of a char, there will be k - 1 substrings that are sequential characters. Instead of counting
+  // along the way, I build this result after gathering the sequences.
   return results.reduce((acc, seq) => {
     while (seq > 0) {
       acc.push(start.repeat(seq) + end.repeat(seq));
